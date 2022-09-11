a6f1b86eb5843f1be01051a96466309f19a3c6c5
Jefry <92845206+JefryLim@users.noreply.github.com>
2022-09-11 15:23:48 +0700

    17:13 YT

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
+import { Button, styled } from "@mui/material";
 
 function App() {
+
+  const BlueButton = styled(Button)(({theme})=>({
+    backgroundColor: theme.palette.otherColor.main,
+    color: "#888",
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "lightblue",
+    },
+    "&:disabled": {
+      backgroundColor: "gray",
+      color: "white",
+    }
+  }));
+
   return (
     <div>
-      Sekarang berubah jadi yang baru
+      <Button variant="text" >Text</Button>
+      <Button variant="contained" color="primary">Contained</Button>
+      <Button variant="outlined" color="primary">Outlined</Button>
+      <Button href="#text-buttons">Link</Button>
+      <Button variant="contained" size="small" disableElevation>
+        Disable elevation
+      </Button>
+
+      <Button variant="contained" sx={{
+        backgroundColor: "skyblue",
+        color: "#888",
+        margin: 5,
+        "&:hover": {
+          backgroundColor: "lightblue",
+        },
+        "&:disabled": {
+          backgroundColor: "gray",
+          color: "white",
+        }
+      }}>
+        wew
+      </Button>
+
+      <BlueButton>
+        wew 2
+      </BlueButton>
     </div>
   );
 }
